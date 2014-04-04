5be6eb2c696b9585153f8f0afd836181b79c7b6d
Elia Schito <elia@schito.me>
2014-04-05 00:29:10 +0200

    Add require support to runtime/kernel

--- opal/corelib/variables.rb
+++ opal/corelib/variables.rb
@@ -1,9 +1,9 @@
 # regexp matches
 $& = $~ = $` = $' = nil
 
-# stub library path
-$: = []
-$" = []
+# requires
+$LOADED_FEATURES = $" = `Opal.loaded_features`
+$LOAD_PATHS      = $: = []
 
 # split lines
 $/ = "\n"
@@ -22,3 +22,4 @@ RUBY_ENGINE         = 'opal'
 RUBY_VERSION        = '2.0.0'
 RUBY_ENGINE_VERSION = '0.6.0'
 RUBY_RELEASE_DATE   = '2014-03-05'
+
